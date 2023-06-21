96373e4bf507a1ef65e1975fb2b0242843e5636b
el-brown <zakariaayoub366@gmail.com>
2023-06-20 18:29:00 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,14 @@ int print_last_digit(int n)
 	int m;
 
 	m = (n % 10);
-	_putchar(m + 48);
-	return (m);
+	if (m > 0)
+	{
+		_putchar(m + 48);
+		return (m);
+	}
+	else
+	{
+		_putchar(-m + 48);
+		return (-m);
+	}
 }
