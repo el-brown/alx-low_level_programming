4eabd62ab3048c062ca20213510efd966a7510ec
el-brown <zakariaayoub366@gmail.com>
2023-06-21 11:02:59 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,21 +15,32 @@ void times_table(void)
 			if (m == 0)
 			{
 				_putchar(48);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			else if (m == 1)
 			{
 				_putchar(n + 48);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
 				if ((o / 10) == 0)
 				{
 					_putchar((o % 10) + 48);
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 				else
 				{
 				_putchar((o / 10) + 48);
 				_putchar((o % 10) + 48);
+				_putchar(',');
+				_putchar(' ');
 				}
 			}
 			if (n == 9)
@@ -37,8 +48,6 @@ void times_table(void)
 				_putchar('\n');
 				continue;
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
 	}
 }
