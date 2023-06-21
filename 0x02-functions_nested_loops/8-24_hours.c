f93a0b72ef371035c0a4c900df5e0de908288789
el-brown <zakariaayoub366@gmail.com>
2023-06-20 18:44:40 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+  *jack_bauer - 24 hours function
+  *Return: void
+  */
+void jack_bauer(void)
+{
+	int m;
+	int n;
+	int o;
+	int p;
+
+	for (m = 48; m <= 50; m++)
+	{
+		for (n = 48; n <= 57; n++)
+		{
+			for (o = 48; o <= 53; o++)
+			{
+				for (p = 48; p <= 57; p++)
+				{
+					_putchar(m);
+					_putchar(n);
+					_putchar(':');
+					_putchar(o);
+					_putchar(p);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
