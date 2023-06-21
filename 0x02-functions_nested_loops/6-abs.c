820816fa5d9f508021a460c1f16848bb67833123
el-brown <zakariaayoub366@gmail.com>
2023-06-20 18:06:33 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_abs - function that prints the absolute value of an int
+  *Return: int
+  *@n: int
+  */
+int _abs(int n)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
