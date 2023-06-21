e942061106481c59f78073c0204f937ef9062859
el-brown <zakariaayoub366@gmail.com>
2023-06-20 19:21:05 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,12 +6,13 @@
 void times_table(void)
 {
 	int m, n;
+	int o = (m * n)
 
 	for (m = 48; m <= 57; m++)
 	{
 		for (n = 48; n <= 57; n++)
 		{
-			_putchar(m * n);
+			_putchar(o);
 			if (n == 57)
 			{
 				continue;
