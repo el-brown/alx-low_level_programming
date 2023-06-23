159357d8206797e50eadf5a4a6e38963af863e29
el-brown <zakariaayoub366@gmail.com>
2023-06-22 17:56:02 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *more_numbers - print to 14 10 times
+  */
+void more_numbers(void)
+{
+	int m;
+	int n;
+	int o;
+
+	for (m = 0; m <= 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar(49);
+			}
+			o = n % 10;
+			_putchar(o + 48);
+		}
+		_putchar('\n');
+	}
+}
