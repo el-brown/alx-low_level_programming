a80af682a531e9d0257ce31921f2fda0f36685d9
el-brown <zakariaayoub366@gmail.com>
2023-07-04 12:03:31 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (c == s[i])
 		{
-			return (s + i);
+			p = & s[i]
+			return (p);
 		}
 	}
 	return (NULL);
