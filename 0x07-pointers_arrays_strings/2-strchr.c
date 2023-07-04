baa8c04665329b5e376db1ef6f1d5f72fd70f988
el-brown <zakariaayoub366@gmail.com>
2023-07-04 12:01:13 -0700

    correctt

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
   * *_strchr - locates char
   *Return: char
@@ -8,14 +9,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (c == s[i])
 		{
-			p = &s[i];
-			return (p);
+			return (s + i);
 		}
 	}
 	return (NULL);
