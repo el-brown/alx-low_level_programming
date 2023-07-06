b20e65bbed03ba2ac85bbd5f8f89ec2c12588ab1
el-brown <zakariaayoub366@gmail.com>
2023-07-06 10:19:28 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,13 +3,33 @@
   *_sqrt_recursion - returns square root of a given num
   *Return: int
   *@n: argument
+  *@i: argument
   */
+int square_root(int n, int i);
+
 int _sqrt_recursion(int n)
 {
-	if ()
+	return (square_root(n, 1));
+}
+
+/**
+  *square_root - calculates a square root of a given num
+  *Return: int
+  *@i: argument
+  *@n: argument
+  */
+int square_root(int n, int i)
+{
+	if (i * i == n)
 	{
+		return (i);
+	}
+	else if (i * i < n)
+	{
+		return (square_root(n, i + 1));
 	}
 	else
 	{
+		return (-1);
 	}
 }
