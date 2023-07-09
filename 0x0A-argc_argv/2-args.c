7c2e7e9145ba969af8bb7886d47d6c43b98a8de9
el-brown <zakariaayoub366@gmail.com>
2023-07-09 07:51:53 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,12 +3,16 @@
 /**
   *main - entry point
   *Return: always 0
-  *@argc: argument couter
+  *@argc: argument counter
   *@argv: argument value
   */
 int main(int argc, char *argv[])
 {
-	(void)argv;
-	printf("%d\n", argc - 1);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
