352ed375b9e97d0ef90742e70b4a49f174b6dbff
el-brown <zakariaayoub366@gmail.com>
2023-07-11 19:39:57 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = 0;
+		*s1 = 0;
 	}
 	else if (s2 == NULL)
 	{
-		s2 = 0;
+		*s2 = 0;
 	}
 	else if (s1 == NULL && s2 == NULL)
 	{
