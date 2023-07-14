1ad0223a4163df0c5e080dad01d3105ccbd0439a
el-brown <zakariaayoub366@gmail.com>
2023-07-13 18:00:28 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,9 +40,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (j = 0; j <= n; j++)
 	{
-		p[i] = s2[j];
-		i++;
+		p[i + j] = s2[j];
 	}
-	p[i] = '\0';
+	p[i + j + 1] = '\0';
 	return (p);
 }
