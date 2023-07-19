12d8e3b3ba16653bdf8f9d624e8166e8d16193fe
el-brown <zakariaayoub366@gmail.com>
2023-07-18 19:01:36 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - a dog struct
+  *@name: its name
+  *@age: its age
+  *@owner: its owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
