4438287298db6641aab90fa8c955216bd195a618
el-brown <zakariaayoub366@gmail.com>
2023-07-27 18:28:22 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *_strlen - length of a string
+  *@s: string
+  *@Return: int
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if(!s)
+	{
+		return (0);
+	}
+	while(*s++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+  *print_list - prinys list
+  *Return: size_t
+  *@h: argument
+  */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", _strlen(h->str), s->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
