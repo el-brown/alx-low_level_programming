9335c3451317f774f513c22e525f6802f7c7a830
el-brown <zakariaayoub366@gmail.com>
2023-08-04 15:35:00 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_abs - function that prints the absolute value of an int
+  *Return: int
+  *@n: int
+  */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
