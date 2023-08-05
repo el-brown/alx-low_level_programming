7d09ae08a20911dd2c376cd9c89c1f3ac6f44fc6
el-brown <zakariaayoub366@gmail.com>
2023-08-04 17:25:07 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0
+*@argc: argument
+*@argv: argument
+*/
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
