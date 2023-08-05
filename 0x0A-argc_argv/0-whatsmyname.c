685722f8699ea19e8c3002ea76438393ff2d7d07
el-brown <zakariaayoub366@gmail.com>
2023-08-04 17:17:14 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - print files name
+*Return: 0
+*@argv: argument
+*@argc: argument
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
