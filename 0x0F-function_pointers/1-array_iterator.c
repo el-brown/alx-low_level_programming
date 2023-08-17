f726436d2f1782301b603e6e8c531edb9c77718b
el-brown <zakariaayoub366@gmail.com>
2023-08-16 18:15:23 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+  *array_iterator - array iterator
+  *Return: void
+  *@array: argument
+  *@size: argument
+  *@action: argument
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
