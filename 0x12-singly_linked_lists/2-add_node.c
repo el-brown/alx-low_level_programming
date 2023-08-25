89d83ae4db06528ebee032167116015cccc9582a
el-brown <zakariaayoub366@gmail.com>
2023-08-25 15:52:10 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+  *add_node - adds anode at the beginning of the list
+  *Return: pointer to the new node
+  *@head: argument
+  *@str: argument
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr;
+	char *str1 = strdup(str);
+	unsigned int i;
+
+	for (i = 0; str1[i] != '\0'; i++)
+		;
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->str = str1;
+	ptr->len = i;
+	ptr->next = NULL;
+
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
+}
