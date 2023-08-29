c63d4674e76fd8d3b9839350493fd820723752be
el-brown <zakariaayoub366@gmail.com>
2023-08-29 16:55:12 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	listint_s *ptr = h;
 	size_t  count = 0;
 
-	while (ptr != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", ptr->n);
+		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
