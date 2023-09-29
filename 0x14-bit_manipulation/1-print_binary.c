a7191a94a4a0767548961fdcccd2cfe1f0c0d87c
zakariaayoub366@gmail.com <zakariaayoub366@gmail.com>
2023-09-28 19:50:24 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+ * print_binary - print binary
+ * Return: void
+ * @n: argument
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i = sizeof(n) * 8, p = 0;
+
+	while (i)
+	{
+		if (n & 1L << --i)
+		{
+			_putchar('1');
+			p++;
+		}
+		else if (p)
+		{
+			_putchar('0');
+		}
+	}
+	if (!p)
+	{
+		_putchar('0');
+	}
+}
