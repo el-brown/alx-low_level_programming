d2f2a9eee42027370be39bb01e6d8854297a3eb0
zakariaayoub366@gmail.com <zakariaayoub366@gmail.com>
2023-10-03 19:06:08 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main,h"
+
+/**
+ * append_text_to_file - append text
+ * Return: int
+ * @filename: argument
+ * @letters: argument
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t i;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	i = read(fd, &buf[0], letters);
+	i = write(STDOUT_FILEENO, &buf[0], i);
+	close(fd);
+	return (i);
+}
