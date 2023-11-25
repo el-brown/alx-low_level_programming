eda97e031ac4e1d7c60e917dd696e4f81246283d
el-brown <zakariaayoub366@gmail.com>
2023-11-24 16:29:13 -0800

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a binary
+ * Return: void
+ * @n: argument
+ */
+
+void print_binary(unsigned long int n)
+{
+	int b = sizeof(n) * 8, p = 0;
+
+	while (b)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			p++;
+		}
+		else if (p)
+		{
+			_putchar('0');
+		}
+	}
+	if (!p)
+	{
+		_putchar('0');
+	}
+}
